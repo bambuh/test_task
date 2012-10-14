d836efcb265f02afa939461e177f781602ce4cb4
Bambuh <a.buhantsov@gmail.com>
2012-10-14 12:30:33 +0300

    Making template

---
+++ mainjs.js
@@ -0,0 +1,8 @@
+			$(function(){
+				$('dl.tabs dt').click(function(){
+					$(this)
+						.siblings().removeClass('selected').end()
+						.next('dd').andSelf().addClass('selected');
+				});
+			});
+		
