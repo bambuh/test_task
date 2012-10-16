900e90ebfc478c69b04ad8e54b2530e1fbf906b7
Bambuh <a.buhantsov@gmail.com>
2012-10-16 16:06:27 +0300

    View, drag...

--- static/mainjs.js
+++ static/mainjs.js
@@ -1,8 +1,39 @@
-			$(function(){
-				$('dl.tabs dt').click(function(){
-					$(this)
-						.siblings().removeClass('selected').end()
-						.next('dd').andSelf().addClass('selected');
-				});
-			});
-		
+$(document).ready(function(){              
+    $('#book_find').click(function(){      
+        $('#books_list').load('/book-filter?bfilter='+$('#book_find_text').attr('value'));           
+    }) 
+    $('#author_find').click(function(){      
+        $('#authors_list').load('/author-filter?afilter='+$('#author_find_text').attr('value'));           
+    }) 
+    // $('#book_find_text').keyup(function(){      
+    //     $('#books_list').load('/book-filter?bfilter='+$('#book_find_text').attr('value'));           
+    // }) 
+    $('#editmode').click(function(){   
+    	$(this).hide();
+    	$('#book_add_div').show();
+    	$('#viewmode').show();
+        $('.books_tab').animate({ width: "59%" }, "slow");
+        $('.authors_tab').show().animate({ opacity: "0"}, "slow").animate({ opacity: "1"}, "slow");
+    }); 
+    $('#viewmode').click(function(){   
+    	$(this).hide();
+    	$('#book_add_div').hide();
+    	$('#editmode').show();
+        $('.authors_tab').animate({ opacity: "0"}, "slow").end().hide();
+        $('.books_tab').animate({ opacity: "1"}, "slow").animate({ width: "100%" }, "slow");
+    }); 
+	$('.book_author').draggable({
+			handle: "span",
+	        helper : 'clone',
+	        opacity : 0.3
+	});
+
+}); 
+		// 	$(function(){
+		// 		$('dl.tabs dt').click(function(){
+		// 			$(this)
+		// 				.siblings().removeClass('selected').end()
+		// 				.next('dd').andSelf().addClass('selected');
+		// 		});
+		// 	});
+		// 
--- temp.py
+++ temp.py
@@ -62,6 +62,10 @@ def login():
     print '!!!'
     return redirect(url_for('index'))
 
+def user(_session):
+    if 'username' in session: return session['username']
+    else: return None
+
 @app.route('/logout')
 def logout():
     session.pop('username', None)
@@ -69,16 +73,25 @@ def logout():
 
 @app.route('/')
 def index():
-    if 'username' in session: user = session['username']
-    else: user = None
-    return render_template('hello2.html', user = user)
+    # if 'username' in session: user = session['username']
+    # else: user = None
+    return render_template('hello2.html', user = user(session))
 
 @app.route('/book-filter')
 def book_filter():
     b_filter = request.args.get('bfilter', '')
     books = [book for book in Book.query.all() if b_filter.lower() in book.name.lower()+str(book.authors).lower()]
-    print 'BOOOOKS', books
-    return redirect(url_for('index'))
+    print 'BOOOOKS',b_filter.lower(),book.name.lower()+str(book.authors).lower(), books
+    return render_template('books_list.html', books = books, user = user(session))
+    # return redirect(url_for('index'))
+
+@app.route('/author-filter')
+def author_filter():
+    a_filter = request.args.get('afilter', '')
+    authors = [author for author in Author.query.all() if a_filter.lower() in author.name.lower()]
+    print "aaaa", authors
+    return render_template('authors_list.html', authors = authors, user = user(session))
+    # return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
